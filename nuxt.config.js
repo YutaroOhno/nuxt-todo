4cf4efbe6c1733bf8c3a01d5c21073134877a622
U <U@yutaronoMacBook-Pro.local>
2020-11-05 19:53:00 +0900

    add ts type check

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,4 +31,12 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  typescript: {
+    typeCheck: {
+      eslint: {
+        files: './**/*.{ts,js,vue}'
+      }
+    }
+  }
 }
